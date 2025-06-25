cfc149e3942862a44ba10f6870adc332f0557245
Francesco Berisso <francesco.berisso.14-2@accademiadigitaleliguria.it>
2025-06-25 15:31:56 +0200

    #1 modifica progetto calcolatrice

---
+++ Progetto/ConsoleApp1/Test_Calcolatrice/Test1.cs
@@ -0,0 +1,27 @@
+﻿using CalcolatriceLib;
+namespace Test_Calcolatrice
+
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestSomma()
+        {
+            //crea test per metodo somma della calcolatrice
+
+            Calcolatrice calcolatrice = new Calcolatrice();
+            double risultato = calcolatrice.Somma(2, 3);
+            Assert.AreEqual(5, risultato, "La somma di 2 e 3 dovrebbe essere 5.");
+        }
+
+        [TestMethod]
+        public void TestSommaConZero()
+        {
+            //crea test per metodo somma della calcolatrice con zero
+            Calcolatrice calcolatrice = new Calcolatrice();
+            double risultato = calcolatrice.Somma(0, 3);
+            Assert.AreEqual(3, risultato, "La somma di 0 e 3 dovrebbe essere 3.");
+    }   
+}
+}
